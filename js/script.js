45fcc65349f0dd4f70668a7e0586ed3e084e95ad
fcarrara24 <f.carrara24@studenti.unibg.it>
2023-10-24 15:27:18 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -27,3 +27,85 @@ guestsInfo.forEach(singleInfo => {
     `)
     console.groupEnd(singleInfo)
 });
+
+
+//////////////
+//  SNACK 2 //
+//////////////
+
+const studentList = [
+    {
+        Id: "213",
+        Name: "Marco Della Rovere",
+        Grades: "78",
+    },
+    {
+        Id: "110",
+        Name: "Paola Cortellassa",
+        Grades: "96",
+    },
+    {
+        Id: "68",
+        Name: "Tizio caio",
+        Grades: "68",
+    },
+    {
+        Id: "567",
+        Name: "Mario Monti",
+        Grades: "70",
+    },
+    {
+        Id: "789",
+        Name: "Fiumi Laghi",
+        Grades: "58",
+    },
+    {
+        Id: "432",
+        Name: "Prati a Volontà",
+        Grades: "96",
+    },
+    {
+        Id: "89",
+        Name: "Fiori nei giardini",
+        Grades: "69",
+    },
+    {
+        Id: "77",
+        Name: "Che Varieta",
+        Grades: "77",
+    },
+]
+
+// aula 
+
+const aula = studentList.map((student) => {
+    return student.Name.toUpperCase();
+});
+
+aula.forEach((student) => {
+    console.log(student)
+});
+
+
+// best group 
+const bestStudents = studentList.filter((specialStudent) => {
+    if (specialStudent.Grades > 70) {
+        return specialStudent;
+    }
+});
+
+bestStudents.forEach((student) => {
+    console.log(student)
+});
+console.log('//////////////////////////')
+
+const newestBestStudents = bestStudents.filter((young) => {
+    if (young.Id > 120) {
+        return young;
+    }
+})
+
+newestBestStudents.forEach((student) => {
+    console.log(student)
+});
+console.log('//////////////////////////')
